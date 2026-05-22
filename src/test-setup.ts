7e1a262e1f0d4f235a076be5eb2e90a29b02c24d
ChristopherMeek <chris@sensemaking.uk.com>
2026-05-22 18:51:57 +0100

    chore: silence VexFlow canvas warnings in test setup

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -1,1 +1,5 @@
 import "@testing-library/jest-dom";
+
+// Silence jsdom's "Not implemented: HTMLCanvasElement.getContext()" warnings
+// triggered by VexFlow's canvas detection at import time.
+HTMLCanvasElement.prototype.getContext = () => null;
